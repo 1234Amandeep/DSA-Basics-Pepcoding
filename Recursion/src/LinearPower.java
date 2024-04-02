cb19e50a2355ab444655654034f9d840efe7ef3e
Amandeep Singh <samandeep8057@gmail.com>
2024-04-02 12:54:40 +0530

    Solved 2 recursion problems, Linear Power calculator, and Lograthmic power calculator and also Understood how 2 recursion calls execute in one go using Euler's diagram

--- Recursion/src/LinearPower.java
+++ Recursion/src/LinearPower.java
@@ -1,25 +1,27 @@
 import java.util.*;
 
-public class Factorial {
+public class LinearPower {
   public static void main(String[] args) throws Exception {
     // 🔥🔥🔥 write code from here...
     try(Scanner scn = new Scanner(System.in))
     {
+      int x = scn.nextInt();
       int n = scn.nextInt();
 
       // 📢📢📢 fn Call
-      System.out.println(factorial(n));
+      System.out.println(power(x, n));
     }
   }
 
   // 🪄🪄🪄 function signature
-  public static int factorial(int n)
+  public static int power(int x, int n)
   {
     // 🔑🔑🔑 logic
-    if(n == 1)
+    if(n == 0)
     {
       return 1;
     }
-    return n * factorial(n - 1);
+    
+    return x * power(x, n - 1);
   }
 }
--- Recursion/src/LogPower.java
+++ Recursion/src/LogPower.java
@@ -1,25 +1,35 @@
 import java.util.*;
 
-public class Factorial {
+public class LogPower {
   public static void main(String[] args) throws Exception {
     // 🔥🔥🔥 write code from here...
     try(Scanner scn = new Scanner(System.in))
     {
+      int x = scn.nextInt();
       int n = scn.nextInt();
 
       // 📢📢📢 fn Call
-      System.out.println(factorial(n));
+      System.out.println(power(x, n));
     }
   }
 
   // 🪄🪄🪄 function signature
-  public static int factorial(int n)
+  public static int power(int x, int n)
   {
     // 🔑🔑🔑 logic
-    if(n == 1)
+    if(n == 0)
     {
       return 1;
     }
-    return n * factorial(n - 1);
+
+    int xnb2 = power(x, n / 2);
+    int xn = xnb2 * xnb2;
+
+    if(n % 2 == 1)
+    {
+      xn = x * xn;
+    }
+    
+    return xn;
   }
 }
